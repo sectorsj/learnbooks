05fc769f25b1c7929e548a6671ec231a9d0abb50
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2024-03-23 11:15:02 +0300

    learn 6.1.9 cloning objects, add CloneTest.java. Employee.java

---
+++ hortsmann_vol1/src/main/java/ru/sectorsj/_295_clone/CloneTest.java
@@ -0,0 +1,16 @@
+package ru.sectorsj._295_clone;
+
+public class CloneTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        var original = new Employee("Винни Ту", 50000);
+
+        original.setHireDay(2000, 1, 1);
+
+        Employee copy = original.clone();
+        copy.raiseSalary(10);
+        copy.setHireDay(2002, 12, 31);
+
+        System.out.println("original = " + original);
+        System.out.println("copy = " + copy);
+    }
+}
---
+++ hortsmann_vol1/src/main/java/ru/sectorsj/_295_clone/Employee.java
@@ -0,0 +1,49 @@
+package ru.sectorsj._295_clone;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+class Employee implements Cloneable {
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        hireDay = new Date();
+    }
+
+    public Employee clone() throws CloneNotSupportedException {
+
+        // вызываем метод Object.clone()
+        Employee cloned = (Employee) super.clone();
+
+        // клонируем изменяемы поля
+        cloned.hireDay = (Date) hireDay.clone();
+        return cloned;
+    }
+
+    /**
+     Устанавливаем дату приема на работу
+     * @param year Год приема на работу
+     * @param month Месяц приема на работу
+     * @param day День приема на работу
+     */
+    public void setHireDay(int year, int month, int day) {
+        Date newHireDay = new GregorianCalendar(year, month - 1, day).getTime();
+
+        //Пример изменения поля экземпляра
+        hireDay.setTime(newHireDay.getTime());
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 10;
+        salary += raise;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[ name: " + name + ", salary: " + salary + ", hireDay: " + hireDay + " ]";
+    }
+}
