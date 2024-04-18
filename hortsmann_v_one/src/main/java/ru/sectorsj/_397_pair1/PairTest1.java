2eb789c0be6526f8c2efdc84d3fb892b55d6e5ae
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2024-04-18 20:14:52 +0300

    chance Pair.java in package _397_pair1

--- hortsmann_v_one/src/main/java/ru/sectorsj/_397_pair1/PairTest1.java
+++ hortsmann_v_one/src/main/java/ru/sectorsj/_397_pair1/PairTest1.java
@@ -36,7 +36,7 @@ class  ArrayAlg {
         if (a == null || a.length == 0) return null;
         String min = a[0];
         String max = a[0];
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 1; i < a.length; i++) {
             if (min.compareTo(a[i]) > 0) min = a[i];
             if (max.compareTo(a[i]) < 0) max = a[i];
         }
