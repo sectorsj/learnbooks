ca81617e4111d4aa38f35992497c68979e06018e
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2024-07-18 14:04:37 +0300

    18.07.24 learn: 10.2.1 Creating a Frame, change: draftnote.txt added: SimpleFrameTest.java, pic_10_1.JPG

---
+++ hortsmann_v_one/src/main/java/ru/sectorsj/_512_simpleframe/SimpleFrameTest.java
@@ -0,0 +1,23 @@
+package ru.sectorsj._512_simpleframe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SimpleFrameTest {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            var frame = new SimpleFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class SimpleFrame extends JFrame {
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public SimpleFrame(){
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+}
