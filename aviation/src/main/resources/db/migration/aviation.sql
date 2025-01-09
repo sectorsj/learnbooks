6bfbd07d4fc798a8d3ca141c40936da6a405b6b6
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-01-09 12:15:51 +0300

    09.01.25 learned: chapter 3 changed: - added: aviation.sql, draftnote_aviation.txt removed: -

---
+++ aviation/src/main/resources/db/migration/aviation.sql
@@ -0,0 +1,12 @@
+INSERT INTO aircrafts (aircraft_code, model, range)
+VALUES ('773', 'Boing 777-300', '11100'),
+        ('763', 'Boing 767-300', '7900'),
+        ('733', 'Boing 737-300', '4200'),
+        ('320', 'Airbus A320-200', '5700'),
+        ('321', 'Airbus A321-200', '5600'),
+        ('319', 'Airbus A319-100', '6700'),
+        ('CN1', 'Cessna 208 Caravan', '1200'),
+        ('CR2', 'Bombardier CRJ-200', '2700');
+
+SELECT * FROM aircrafts;
+
