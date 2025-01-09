1f01c5eeee4921059b57cebc150c1d0dcd5279f9
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2025-01-09 13:25:10 +0300

    09.01.25 learned: chapter 3: update, delete commands changed: aviation_db.sql, draftnote_aviation_db.txt added: - removed: -

--- aviation/src/main/resources/db/migration/aviation_db.sql
+++ aviation/src/main/resources/db/migration/aviation_db.sql
@@ -1,12 +1,25 @@
+DROP TABLE aircrafts;
+
+CREATE TABLE aircrafts
+( aircraft_code char(3) NOT NULL,
+  model text NOT NULL,
+  range integer NOT NULL,
+  CHECK (range > 0),
+  PRIMARY KEY (aircraft_code)
+);
+
 INSERT INTO aircrafts (aircraft_code, model, range)
-VALUES ('773', 'Boeing 777-300', '11100'),
-        ('763', 'Boeing 767-300', '7900'),
-        ('733', 'Boeing 737-300', '4200'),
-        ('320', 'Airbus A320-200', '5700'),
-        ('321', 'Airbus A321-200', '5600'),
-        ('319', 'Airbus A319-100', '6700'),
-        ('CN1', 'Cessna 208 Caravan', '1200'),
-        ('CR2', 'Bombardier CRJ-200', '2700');
+    VALUES ('SU9', 'Sukhoi SuperJet-100', '3000');
+
+INSERT INTO aircrafts (aircraft_code, model, range)
+    VALUES ('773', 'Boeing 777-300', '11100'),
+           ('763', 'Boeing 767-300', '7900'),
+           ('733', 'Boeing 737-300', '4200'),
+           ('320', 'Airbus A320-200', '5700'),
+           ('321', 'Airbus A321-200', '5600'),
+           ('319', 'Airbus A319-100', '6700'),
+           ('CN1', 'Cessna 208 Caravan', '1200'),
+           ('CR2', 'Bombardier CRJ-200', '2700');
 
 SELECT * FROM aircrafts;
 
@@ -17,3 +30,12 @@ SELECT model, aircraft_code, range
 SELECT model, aircraft_code, range
     FROM aircrafts
     WHERE range >= 4000 AND range <= 6000;
+
+UPDATE aircrafts
+    SET range = 3500
+    WHERE aircraft_code = 'SU9';
+
+SELECT *
+    FROM aircrafts
+    WHERE aircraft_code = 'SU9';
+
